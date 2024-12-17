b8b02742538e1ac4a8dc23ce47fdd23376b76628
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-12-17 13:06:35 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState ,React } from 'react'
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Sidebar/Sidebar'
 import Home from './components/Home/Home'
 import InfiniteScroll from './components/Home/InfiniteScroll'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Navbar />
+      <Sidebar />
       {/* <Home /> */}
       {/* <InfiniteScroll /> */}
     </div>
